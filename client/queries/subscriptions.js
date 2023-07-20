2cdf9f926e7a1be1efd82ff947c1ba2e4098c966
Zhivko Nikolov <zhivko.nikolov@yara.com>
2023-07-20 12:57:55 +0300

    test branch created (#1)

---
+++ client/queries/subscriptions.js
@@ -0,0 +1,10 @@
+import { gql } from '@apollo/client';
+
+export const SONGS_SUBSCRIPTION = gql`
+  subscription OnCommentAdded($postID: ID!) {
+    commentAdded(postID: $postID) {
+      id
+      content
+    }
+  }
+`;
--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const app = require('./server/server');
 app.listen(4000, () => {
   console.log('Listening');
 });
+
+
+// change 1
+// change 2
+// change 3
